185ce8b2842a9a29ab91d8ca6c91b3b338443890
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-11 11:34:32 +0530

    [HEMANT]Add: CHECK ATM DEPOSITE,WIDTHRAWAL&BALANCE

---
+++ ATMMACHINE/ATM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class ATM
+    {
+        int balance = 5000;
+        int withdrwal = 0;
+        int amount = 0;
+        public void CHECKBALANCE()
+        {
+            Console.WriteLine("YOUR BALANCE = " + balance);
+        }
+        public void CheckWidthrwal()
+        {
+            if (balance == 0)
+            {
+                Console.WriteLine("NO BALANCE AVAILABLE");
+
+            }
+            else
+            {
+                Console.WriteLine("ENTER THE AMOUNT");
+                amount = Convert.ToInt32(Console.ReadLine());
+                if (amount > balance)
+                {
+                    Console.WriteLine("Insufficient balance. Unable to perform withdrawal.");
+                }
+                else
+                {
+                    withdrwal = amount;
+                    balance -= amount;
+                    Console.WriteLine("Withdrawal Successful");
+                }
+            }
+        }
+        public void CheckDeposit()
+        {
+            Console.WriteLine("ENTER THE AMOUNT TO DEPOSIT");
+            amount = Convert.ToInt32(Console.ReadLine());
+            balance += amount;
+            Console.WriteLine("Deposit Successful");
+        }
+
+    }
+}
---
+++ ATMMACHINE/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM atm = new ATM();
+            Console.WriteLine("welcome to ATM");
+            Console.WriteLine("ENTER YOUR 4 Digit PIN");
+            string pin = Console.ReadLine();
+            int num = Convert.ToInt32(pin.Length);
+            while (true)
+            {
+
+                if (num == 4)
+                {
+                    Console.WriteLine("1. Check Balance");
+                    Console.WriteLine("2. Deposite");
+                    Console.WriteLine("3. Widthrwal");
+                    Console.WriteLine("ENTER OPTION");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            atm.CHECKBALANCE();
+                            break;
+                        case 2:
+                            atm.CheckDeposit();
+                            atm.CHECKBALANCE();
+                            break;
+                        case 3:
+                            atm.CheckWidthrwal();
+                            atm.CHECKBALANCE();
+                            break;
+                        default:
+                            Console.WriteLine("INVALID INPUT");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("please enter the 4 digit pin");
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
